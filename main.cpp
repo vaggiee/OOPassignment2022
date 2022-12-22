b4f40e45bb84ad65b4635476cfef2c7218d077ec
ppetros1 <100340371+ppetros1@users.noreply.github.com>
2022-12-22 19:54:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main() {
 	int x, y;
 	char tteam;
-	srand(time(NULL));
+	srand(time(0));
 	
 	cout << "To WvV jekinhse! Gia na kaneis pause pata P kai to esc gia na termatisei to paixnidi"<<endl;
 	cout << "Dwse diastaseis tou xarth" << endl;
@@ -28,28 +28,53 @@ int main() {
 		arrayV[i] = new monsters(2);
 	}
 	
-	avatar mya(0,tteam);
+	avatar* mya=new avatar(0,tteam);
 	map *map1=new map;
 	
 	map1->create_map(x,y);
-	map1->set_map(x,y,mya,arrayW,arrayV,N);
+	map1->set_map(x,y,*mya,arrayW,arrayV,N);
 
 	while (true) {
 	// Clear the screen
 		system("cls");
 		map1->printmap(x,y);
+
 		map1->check_neigh(arrayW, arrayV, x, y, N);
 
-		if(_kbhit() && (_getch() == 'p' || _getch() == 'P'))
+		if (_kbhit())
 		{
-			cout << "To paixnidi brhsketai se paush"<<endl;
-			cout << "Ta nekra vampire einai:" << map1->get_deadV() << endl;
-			cout << "Ta nekra werewolve einai:" << map1->get_deadW() << endl;
-			system("pause");
+			if ((_getch() == 'p' || _getch() == 'P')) {
+				cout << "To paixnidi brhsketai se paush" << endl;
+				cout << "Ta nekra vampire einai:" << map1->get_deadV() << endl;
+				cout << "Ta nekra werewolve einai:" << map1->get_deadW() << endl;
+				system("pause");
+			}
+			if (_getch() == 72) {
+				map1->move_av(*mya, x, y, 0);
+			}
+			if (_getch() == 80) {
+				map1->move_av(*mya, x, y, 1);
+			}
+			if (_getch() == 75) {
+				map1->move_av(*mya, x, y, 2);
+			}
+			if (_getch() == 77) {
+				map1->move_av(*mya, x, y, 3);
+			}
 		}
 		map1->move(arrayW, arrayV,x,y, N);
-		Sleep(500);
+		Sleep(400);
 	}
 
+	//Delete allocated memory
+	for (int i = 0; i < N; i++) {
+		delete arrayW[i];
+	}
+	for (int i = 0; i < N; i++) {
+		delete arrayV[i];
+	}
+	delete [] arrayW;
+	delete [] arrayV;
+	map1->deletemap(x);
 	return 0;
 }
