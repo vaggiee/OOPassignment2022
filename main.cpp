9eb856442dacdc0dccb036c854562b331e0cc655
ppetros1 <100340371+ppetros1@users.noreply.github.com>
2022-12-23 17:16:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,17 +28,19 @@ int main() {
 	cout << " WvV has started !"<<endl<< "Press SPACE to pause and ESC to end the game!"<<endl;
 	cout << "Give map dimensions" << endl;
 	cin >> x >> y;
-	cout << "Choose a team: W or w for werewolves and V or v for vampires"<<endl<<"By pressing left shift you can heal your team!" << endl;
+	cout << "Choose a team: W or w for werewolves and V or v for vampires"<<endl
+		<<"If you got a magic potion,by pressing left shift you can heal your team!" << endl;
 	cin >> tteam;
-	while (true) {
-		if (tteam != 'W' || tteam != 'w' || tteam != 'V' || tteam != 'v') {
-			cout << "Wrong input...";
-			cout << "Choose a team: W or w for werewolves and V or v for vampires" << endl;
-			cin >> tteam;
-			break;
-		}
+	//Checks users input
+	while (tteam != 'W' && tteam != 'w' && tteam != 'V' && tteam != 'v') {
+		cout << "Wrong input...";
+		cout << "Choose a team: W or w for werewolves and V or v for vampires" << endl;
+		cin >> tteam;		
 	}
+
 	int N = (x * y) / 15;
+
+	//Allocate memory for pointer arrays of werewolves and vampires
 	monsters** arrayW = new monsters* [N];
 	monsters** arrayV = new monsters * [N];
 
@@ -49,42 +51,80 @@ int main() {
 		arrayV[i] = new monsters(2);
 	}
 	
-	avatar* mya=new avatar(0,tteam);
+	avatar mya(0,tteam);
 	map *map1=new map;
 	
 	map1->create_map(x,y);
-	map1->set_map(x,y,*mya,arrayW,arrayV,N);
+	map1->set_map(x,y,mya,arrayW,arrayV,N);
 
-	while (true) {
-	// Clear the screen
-		system("cls");
-		map1->printmap(x,y);
-
-		map1->check_neigh(arrayW, arrayV, x, y, N);
-		switch (getKey()) {
-		case VK_LEFT:
-			map1->move_av(*mya, x, y, 2);
-			break;
-		case VK_RIGHT:
-			map1->move_av(*mya, x, y, 3);
-			break;
-		case VK_UP:
-			map1->move_av(*mya, x, y, 0);
-			break;
-		case VK_DOWN:
-			map1->move_av(*mya, x, y, 1);
-			break;
-		case VK_ESCAPE:
-			g = 0;
-			break;
-		case VK_SPACE:
-			cout << endl << "The number of dead werewolves is:"<<map1->get_deadW() << endl;
-			cout << endl << "The number of dead vampires is:" << map1->get_deadV() << endl;
-			system("pause");
-			break;
+	int g = 1;
+	while (g) {
+		//for time<12 it is day and for time>12 we got night
+		for (int time = 0; time < 24,g==1; time++) {
+			// Clear the screen
+			system("cls");
+			//Shows the user if is night or day
+			if (time < 12) {
+				cout << endl << "Its day time"<<endl;
+			}
+			else {
+				cout << endl << "Its night time"<<endl;
+			}
+			if(mya.get_team()=='V' || mya.get_team() == 'v')cout << "You are with the vampires";
+			else{ cout << "Your team is werewolves"; }
+			map1->printmap(x, y);
+			//Geets users input
+			switch (getKey()) {
+			case VK_LEFT:
+				map1->move_av(mya, x, y, 2);
+				break;
+			case VK_RIGHT:
+				map1->move_av(mya, x, y, 3);
+				break;
+			case VK_UP:
+				map1->move_av(mya, x, y, 0);
+				break;
+			case VK_DOWN:
+				map1->move_av(mya, x, y, 1);
+				break;
+			case VK_LSHIFT:
+				if (mya.get_potion() > 0) {
+					if (time>12 && (mya.get_team() == 'W' || mya.get_team() == 'w')) {
+						for (int i = 0; i < N; i++) {
+							arrayW[i]->avatarheal();
+						}
+						mya.decreasepot();
+					}
+					else if (time<12 && (mya.get_team() == 'V' || mya.get_team() == 'v')) {
+						for (int i = 0; i < N; i++) {
+							arrayV[i]->avatarheal();
+						}
+						mya.decreasepot();
+					}
+					else { 
+						cout << "You cant heal your team right now";
+						Sleep(100);
+					}
+				}
+				else {
+					cout << "NOT ENOUGH POTIONS";
+					Sleep(100);
+				}
+				break;
+			case VK_ESCAPE:
+				g = 0;
+				break;
+			case VK_SPACE:
+				cout << endl << "The number of alive werewolves is: " << N - map1->get_deadW() << endl;
+				cout << endl << "The number of alive vampires is: " << N - map1->get_deadV() << endl;
+				cout << endl << "The number of potions is: " << mya.get_potion() << endl;
+				system("pause");
+				break;
+			}
+			map1->check_neigh(arrayW, arrayV, x, y, N);
+			map1->move(arrayW, arrayV, x, y, N);
+			Sleep(50);
 		}
-		map1->move(arrayW, arrayV,x,y, N);
-		Sleep(400);
 	}
 
 	//Delete allocated memory
